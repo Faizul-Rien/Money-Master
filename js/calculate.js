1e37e0c1adfc45cf0e4a8b18e7f34e59bba443e8
Faizul-Hassan-2015 <86608620+Faizul-Hassan-2015@users.noreply.github.com>
2022-02-17 03:53:56 +0600

    two error handler add

--- js/calculate.js
+++ js/calculate.js
@@ -1,7 +1,8 @@
 
-function amountInput(product){
-    const incomeInput = document.getElementById(product +'-input');
+function amountInput(amount){
+    const incomeInput = document.getElementById(amount +'-input');
     const inputValue = parseFloat(incomeInput.value);
+
     return inputValue;
 
     
@@ -10,15 +11,60 @@ function amountInput(product){
 
 document.getElementById('calculate-btn').addEventListener('click',function(){
     
+    const inputValue = amountInput('income');
     const foodValue = amountInput('food');
     const rentValue = amountInput('rent');
     const clothesValue = amountInput('clothes');
 
+    const wrongInput = document.getElementById('failed');
+
+   
+
     const totalExpenses = document.getElementById('total-expenses');
     const totalExpensesValue = foodValue + rentValue + clothesValue;
     totalExpenses.innerText= totalExpensesValue;
+
+    if(totalExpensesValue > inputValue){
+        const BalanceLeft = document.getElementById('balance-left');
+        const balanceValue = inputValue - totalExpensesValue;
+        BalanceLeft.innerText = balanceValue;
+        wrongInput.style.display = 'block';
+    }
    
+    else{
+        const BalanceLeft = document.getElementById('balance-left');
+        const balanceValue = inputValue - totalExpensesValue;
+        BalanceLeft.innerText = balanceValue;
+        wrongInput.style.display = 'none';
 
-
+    }
 })
 
+document.getElementById('save-btn').addEventListener('click',function(){
+    const saveMoney = amountInput('save');
+    const inputValue = amountInput('income')
+
+    const savingAmount = document.getElementById('saving-amount');
+    const saveBalanceAmount = (saveMoney/100)* inputValue ;
+    savingAmount.innerText = saveBalanceAmount;
+
+    const balanceLeft = document.getElementById('balance-left');
+    const balanceValue = balanceLeft.innerText;
+    balanceLeft.innerText = balanceValue;
+
+    const balanceRemaining = document.getElementById('remaining-balance');
+    const worngInput = document.getElementById('money-short');
+
+    if( balanceValue > saveBalanceAmount ){
+        const remainingBalanceValue = balanceValue - saveBalanceAmount;
+        balanceRemaining.innerText = remainingBalanceValue;
+        worngInput.style.display = 'none';
+    
+    }
+    else{
+        const remainingBalanceValue = balanceValue - saveBalanceAmount;
+        balanceRemaining.innerText = remainingBalanceValue;
+        worngInput.style.display = 'block';
+    }
+   
+})
